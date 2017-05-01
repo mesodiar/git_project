e0cdcaa2f34d9205b35544b1be19d65fa5d169bf
mesodiar <mils@prontomarketing.com>
2017-05-01 10:51:40 +0700

    Get data from api :koala:

--- repo/views.py
+++ repo/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 
+from .forms import URLForm
+
 import requests
 
-def repo_home(request):
-    return render(request, 'templates/home.html', {})
+def get_url(request):
+    if request.method == 'POST':
+        form = URLForm(request.POST)
+        if form.is_valid():
+            input_url = form.cleaned_data['input_url']
+            split_url = input_url.split('/')
+            api_url = f'https://api.github.com/repos/{split_url[3]}/{split_url[4]}/contributors'
+            print(api_url)
+            repo = requests.get(api_url)
+    else:
+        form = URLForm()
+    return render(request, 'templates/home.html', {'form': form})
