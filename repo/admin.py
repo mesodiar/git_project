b348eb4994486c521ebe1ffee5726bc171beaa6c
mesodiar <mils@prontomarketing.com>
2017-05-01 12:57:51 +0700

    Show all columns in admin

--- repo/admin.py
+++ repo/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from .models import Repo, Contributor
 
-myModels = [Repo, Contributor]
-admin.site.register(myModels)
+@admin.register(Repo)
+class RepoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner')
+
+
+@admin.register(Contributor)
+class ContributorAdmin(admin.ModelAdmin):
+    list_display = ('username', 'commit_amount', 'repo')
