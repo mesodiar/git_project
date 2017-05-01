ce776b26cea16677e1d186a8e90632e27914741a
mesodiar <mils@prontomarketing.com>
2017-05-01 12:58:12 +0700

    Render repo name and owner in html :koala:

--- repo/views.py
+++ repo/views.py
@@ -1,6 +1,8 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from .forms import URLForm
+from .models import Contributor, Repo
 
 import requests
 
@@ -12,7 +14,13 @@ def get_url(request):
             split_url = input_url.split('/')
             api_url = f'https://api.github.com/repos/{split_url[3]}/{split_url[4]}/contributors'
             print(api_url)
-            repo = requests.get(api_url)
+            repo, created = Repo.objects.get_or_create(
+                name=split_url[3],
+                owner=split_url[4]
+            )
+            data = requests.get(api_url)
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
     else:
         form = URLForm()
-    return render(request, 'templates/home.html', {'form': form})
+        repo_names = Repo.objects.all()
+        return render(request, 'templates/home.html', {'form': form, 'repo_names': repo_names})
